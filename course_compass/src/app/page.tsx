8d7afff679db2d5e506b02073368a2f7eecf2655
Allen Nguyen <anhn321@gmail.com>
2024-06-09 12:25:06 -0700

    feat: install and set up NextUI

--- course_compass/src/app/page.tsx
+++ course_compass/src/app/page.tsx
@@ -1,40 +1,42 @@
-export default function Home() {
-	return (
-		<>
-			<main>
-				<h1>
-					Degree Planning <span>Meets AI.</span>
-				</h1>
-				<p>
-					With CourseCompass, you can plan out your personalized
-					degree in 10 minutes with the help of AI.
-				</p>
-				<button>Get Started!</button>
-			</main>
+import { Button } from '@nextui-org/react';
 
-			<footer>
-				<div>
-					<h3>ACCESS REVIEWS</h3>
-					<p>
-						Gain access to comprehensive course and professor
-						reviews to inform your decisions.
-					</p>
-				</div>
-				<div>
-					<h3>EXPLORE COURSES</h3>
-					<p>
-						Explore a wide range of course offerings scheduled for
-						upcoming semesters.
-					</p>
-				</div>
-				<div>
-					<h3>PLAN YOUR DEGREE</h3>
-					<p>
-						Easily plan your personalized degree trajectory with the
-						assistance of AI.
-					</p>
-				</div>
-			</footer>
-		</>
-	);
+export default function Home() {
+  return (
+    <>
+      <section>
+        <h1>
+          Degree Planning <span>Meets AI.</span>
+        </h1>
+        <p>
+          With CourseCompass, you can plan out your personalized degree in 10
+          minutes with the help of AI.
+        </p>
+        <Button>Get Started!</Button>
+      </section>
+
+      <footer>
+        <div>
+          <h3>ACCESS REVIEWS</h3>
+          <p>
+            Gain access to comprehensive course and professor reviews to inform
+            your decisions.
+          </p>
+        </div>
+        <div>
+          <h3>EXPLORE COURSES</h3>
+          <p>
+            Explore a wide range of course offerings scheduled for upcoming
+            semesters.
+          </p>
+        </div>
+        <div>
+          <h3>PLAN YOUR DEGREE</h3>
+          <p>
+            Easily plan your personalized degree trajectory with the assistance
+            of AI.
+          </p>
+        </div>
+      </footer>
+    </>
+  );
 }
